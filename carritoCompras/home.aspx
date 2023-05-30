48051224c9c15eb6921f3a742e1812b6461634c6
MatiasSilvaIslas <matias.silva@alumnos.frgp.utn.edu.ar>
2023-05-30 20:35:47 -0300

    Agrego lista de articulos al carrito en variable de sesion

--- carritoCompras/home.aspx
+++ carritoCompras/home.aspx
@@ -4,28 +4,45 @@
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <div class="row row-cols-1 row-cols-md-3 g-4">
-         %><%
-            foreach (dominio.Articulo articulo in articulos)
-            { %>        
+        <%--<%
+              foreach (dominio.Articulo articulo in articulos)
+              { %>
 
         <div class="col">
             <div class="card">
                 <img src="<%: articulo.Imagenes.Count > 0 ? articulo.Imagenes[0].ImagenUrl : "https://www.redeszone.net/app/uploads-redeszone.net/2021/09/Error-404-01-e1633683457508.jpg" %>" class="card-img-top" alt="..." />
                 <div class="card-body">
                     <h5 class="card-title"><%: articulo.Nombre  %></h5>
-                    <p class="card-text"> $<%: articulo.Precio %></p>
-                    <a href="Detalle.aspx?id=<%: articulo.Id %>" >Ver detalle </a>
+                    <%string aux = articulo.Id.ToString(); %>
+                    <p class="card-text">$<%: articulo.Precio %></p>
+                    <a href="Detalle.aspx?id=<%: articulo.Id %>">Ver detalle </a>
                     <div class="input-group mb-3">
                         <!--Hay que ver como hacer para que agregar en la sesion, una lista de articulos y se multiplique por la cantidad de artículos ingresados
                             <input type="number" id="cantidad_<%: articulo.Id %>" class="form-control" placeholder="Cantidad" min="1" value="1">
                         <button class="btn btn-success" onclick="agregarAlCarrito('<%: articulo.Id %>')"> -->
-                        <button class="btn btn-success">
-                        <i class="fa fa-shopping-cart"></i> 
-                        </button>
+                        <asp:Button ID="btnAgregar" OnCommand="agregar_Click" runat="server" Text="Agregar" CssClass="btn btn-light" CommandArgument="<# articulo.Id %>" />
                     </div>
                 </div>
-            </div> 
+            </div>
         </div>
-         <% } %>
+        <% } %>--%>
+
+        <asp:Repeater ID="repArticulos" runat="server">
+            <ItemTemplate>
+                <div class="col">
+                    <div class="card">
+                        <img src='<%# ((List<dominio.Imagen>)Eval("Imagenes")).Count > 0 ? ((List<dominio.Imagen>)Eval("Imagenes"))[0].ImagenUrl : "https://www.redeszone.net/app/uploads-redeszone.net/2021/09/Error-404-01-e1633683457508.jpg" %>' class="card-img-top" alt="..." />
+                        <div class="card-body">
+                            <div class="card-body">
+                                <h5 class="card-title"><%#Eval("Nombre") %></h5>
+                                <p class="card-text">$<%#Eval("Precio") %></p>
+                                <a href="Detalle.aspx?id=<%#Eval("Id") %>">Ver detalle </a>
+                                <asp:Button ID="btnAgregar" OnClick="agregar_Click" runat="server" Text="Agregar" CssClass="btn btn-light" CommandArgument='<%#Eval("Id") %>' CommandName="ArticuloId" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </ItemTemplate>
+        </asp:Repeater>
     </div>
 </asp:Content>
--- carritoCompras/home.aspx.cs
+++ carritoCompras/home.aspx.cs
@@ -9,23 +9,85 @@ using negocio;
 
 namespace carritoCompras
 {
+    //public partial class home : System.Web.UI.Page
+    //{
+    //    public List<Articulo> articulos { get; set; }
+    //    //private List<Articulo> articulosCarrito;
+    //    protected void Page_Load(object sender, EventArgs e)
+    //    {
+    //        ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+    //        articulos = articuloNegocio.listar();
+    //        Session["ArticulosCarrito"] = new List<Articulo>(); // Inicializar la lista en la sesión
+
+    //        if (!IsPostBack)
+    //        {
+    //            repArticulos.DataSource = articulos;
+    //            repArticulos.DataBind();
+    //        }
+
+    //    }
+
+    //    protected void agregar_Click(object sender, EventArgs e)
+    //    {
+    //        string idArticulo = ((Button)sender).CommandArgument;
+    //        ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+    //        Articulo articulo = new Articulo();
+    //        articulo = articuloNegocio.listar(Int32.Parse(idArticulo));
+
+    //        List<Articulo> articulosCarrito = (List<Articulo>)Session["ArticulosCarrito"];
+
+    //        // Agregar el artículo a la lista del carrito
+    //        articulosCarrito.Add(articulo);
+
+    //        // Actualizar la lista en la sesión
+    //        Session["ArticulosCarrito"] = articulosCarrito;
+    //    }
+
+    //    protected void btnPrueba_Click(object sender, EventArgs e)
+    //    {
+    //        List<Articulo> ac = (List<Articulo>)Session["ArticulosCarrito"];
+
+    //        lblCantidad.Text = ac.Count.ToString();
+    //    }
+    //}
     public partial class home : System.Web.UI.Page
     {
         public List<Articulo> articulos { get; set; }
+        public List<Articulo> articulosCarrito { get; set; }
+
         protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                CargarArticulos();
+            }
+        }
+
+        protected void CargarArticulos()
         {
             ArticuloNegocio articuloNegocio = new ArticuloNegocio();
             articulos = articuloNegocio.listar();
-           /* if (!IsPostBack)
-            {
-                repArticulos.DataSource = articulos;
-                repArticulos.DataBind();
-            }*/
+            Session["ArticulosCarrito"] = new List<Articulo>();
+
+            repArticulos.DataSource = articulos;
+            repArticulos.DataBind();
         }
-       /* protected void btnDetalle_Click(object sender, EventArgs e)
+
+        protected void agregar_Click(object sender, EventArgs e)
         {
-            string valor = ((Button)sender).CommandArgument;
-        }*/
+            string idArticulo = ((Button)sender).CommandArgument;
+            ArticuloNegocio articuloNegocio = new ArticuloNegocio();
+            Articulo articulo = articuloNegocio.listar(Int32.Parse(idArticulo));//Traigo el articulo desde la DDBB
+
+            List<Articulo> articulosCarrito = (List<Articulo>)Session["ArticulosCarrito"];//obtengo la lista desde la sesion
+
+            // Agregar el artículo a la lista del carrito
+            articulosCarrito.Add(articulo); //Agrego un articulo a la lista
+
+            // Actualizar la lista en la sesión
+            Session["ArticulosCarrito"] = articulosCarrito; //actualizo la lista en la sesion
+
+        }
 
     }
 }
